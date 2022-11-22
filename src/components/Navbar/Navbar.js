7fe6a3d0dd80ddeaee8b7e3407911ef9c09b0ab8
jayjay1878 <jaybartley777@outlook.com>
2022-11-22 14:41:58 +0000

    nav bar
    nav bar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./Navbar.ccs";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div>
+      <img src="/" alt="mascot" />
+      <h1 className="title"> BOOTCAMP BUDDY</h1>
+      <img src="/" alt="mascot" />
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
