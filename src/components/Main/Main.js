fb7222fef5956d577025851c3eeeadbc16aa8a5b
Julio Velezmoro <julio28ve@gmail.com>
2022-11-22 15:47:16 +0000

    main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,18 +2,12 @@ import React from "react";
 import "./Main.css";
 import Wellbeing from "./Wellbeing/Wellbeing.js";
 import QuizzMe from "./QuizzMe";
+import Interview from "./Interview";
 
 function Main() {
   return (
+ 
     <div>
-      ;
-      <Wellbeing />
-      <QuizzMe />
-    </div>
-  );
-}
-    <div>
-      ;
       <Wellbeing />
       <QuizzMe />
       <Interview />
