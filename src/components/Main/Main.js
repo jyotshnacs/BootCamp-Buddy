7dca65c72b806d626cd8a82c1ae47af462a816a2
jo <jyotshnacs@gmail.com>
2022-11-22 15:17:56 +0000

    QuizzMe

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Main.css";
 import Wellbeing from "./Wellbeing/Wellbeing.js";
+import QuizzMe from "./QuizzMe"; 
+
 
 function Main() {
-  return <div></div>;
-  //<Wellbeing />;
-}
+  return (
+  <div>;
+  {/* <Wellbeing /> */}
+  <QuizzMe />
+  </div>
+)};
 
 export default Main;
--- src/components/Main/QuizzMe/QuizzMe.js
+++ src/components/Main/QuizzMe/QuizzMe.js
@@ -1,1 +1,15 @@
-//hello jo!
+import React from 'react';
+
+
+function QuizzMe() {
+  return (
+    
+    <div>
+    <input type = "text" />
+    <img src = "/" alt = "Code snippet"/>
+    <p> Question related to the code block</p>
+    </div>
+  )
+}
+
+export default QuizzMe;
