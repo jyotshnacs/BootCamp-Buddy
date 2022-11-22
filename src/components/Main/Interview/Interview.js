f010ace592dfecce15966744ffaf2e90e44623fb
Bazelden <jack.waller@hotmail.com>
2022-11-22 15:35:12 +0000

    interview component skeleton done

--- src/components/Main/Interview/Interview.js
+++ src/components/Main/Interview/Interview.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function Interview() {
   return (
-    <div>interview</div>
-  )
+    <div>
+      <div>
+        <h1>Interview Prep</h1>
+      </div>
+      <div>
+        <p>
+          This is the text where the interview question or technique to practice
+          will go.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Interview;
