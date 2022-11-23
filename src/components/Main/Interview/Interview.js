889e2c85c958d304d9b038c754c3be31fc89f586
Bazelden <jack.waller@hotmail.com>
2022-11-23 16:24:49 +0000

    Interview prep component working via backend

--- src/components/Main/Interview/Interview.js
+++ src/components/Main/Interview/Interview.js
@@ -3,24 +3,31 @@ import React, { useState, useEffect } from "react";
 function Interview() {
   const [text, setText] = useState("");
 
-  //useEffect(() => {
-  // instead of selecting from an array, need to return the result of fetch request to our API
-  async function fetchData() {
-    //port number hard coded - could pass in the PORT env variable?
-    const response = await fetch(`http://localhost:3005/api/interviewprep`);
-    const data = await response.json();
-    console.log(data);
-  }
+  useEffect(() => {
+    // instead of selecting from an array, need to return the result of fetch request to our API
 
-  //     const question = Math.floor(Math.random() * interviewQuestions.length);
-  //     setText(interviewQuestions[question]);
+    async function fetchData() {
+      //port number hard coded - could pass in the PORT env variable?
+      const response = await fetch(`http://localhost:3005/api/interviewprep`);
+      const data = await response.json();
+      console.log(data);
+      console.log(data.payload[0].prep);
+
+      const question =
+        data.payload[Math.floor(Math.random() * data.payload.length)].prep;
+      setText(question);
+
+      console.log(question);
+    }
+    fetchData();
+  }, []);
+
   //  // }, []);
 
   return (
     <div>
       <div>
         <h1>Interview Prep</h1>
-        <button onClick={fetchData}>Submit</button>
       </div>
       <div>
         <p>{text}</p>
