98299db49ab0675dabb60194fe9baed5d335f5f7
Bazelden <jack.waller@hotmail.com>
2022-11-24 15:28:54 +0000

    radio buttons done and back end information feeding in correctly

--- src/components/Main/QuizzMe/QuizzMe.js
+++ src/components/Main/QuizzMe/QuizzMe.js
@@ -1,87 +1,120 @@
 import React from "react";
-import Dropdown from "./Dropdown/Dropdown";
+// import Dropdown from "./Dropdown/Dropdown";
 import { useState } from "react";
+import "./QuizzMe.css";
 
 function QuizzMe() {
-  const [input, setInput] = useState("");
+  // const [input, setInput] = useState("");
   const [question, setQuestion] = useState("");
+  const [correct, setCorrect] = useState("");
+  const [incorrectA, setIncorrectA] = useState("");
+  const [incorrectB, setIncorrectB] = useState("");
 
   /* 1) Send a fetch request to the Api
    to get the quizQuestion back from the Database.*/
 
-  async function fetchData() {
+  async function fetchQuestion() {
     //port number hard coded - could pass in the PORT env variable?
     const response = await fetch(`http://localhost:3005/api/quizquestions`);
     const data = await response.json();
     setQuestion(data.payload[0].question);
-    console.log(data.payload[0].question);
+    // console.log(data.payload[0].question);
   }
-  fetchData();
+  fetchQuestion();
 
-  const handleMenuOne = () => {
-    console.log("clicked 1");
-  };
+  async function fetchCorrect() {
+    //port number hard coded - could pass in the PORT env variable?
+    const response = await fetch(`http://localhost:3005/api/quizquestions`);
+    const data = await response.json();
+    setCorrect(data.payload[0].correctanswer);
+    // console.log(data.payload[0].correctanswer);
+  }
 
-  const handleMenuTwo = () => {
-    console.log("clicked 2");
-  };
-  const handleMenuThree = () => {
-    console.log("clicked 3");
-  };
-  const handleMenuFour = () => {
-    console.log("clicked 4");
-  };
+  fetchCorrect();
 
-  const rightAnswer = () => {
-    if (input === "1") {
-      alert("Correct");
-    } else {
-      alert("Wrong");
-    }
-  };
-  const handleChange = (e) => {
-    setInput(e.target.value);
-    console.log(input);
-  };
+  async function fetchIncorrectA() {
+    //port number hard coded - could pass in the PORT env variable?
+    const response = await fetch(`http://localhost:3005/api/quizquestions`);
+    const data = await response.json();
+    setIncorrectA(data.payload[0].incorrectanswera);
+    // console.log(data.payload[0].correctanswer);
+  }
 
-  let answers = [1, 2, 3, 4];
+  fetchIncorrectA();
+
+  async function fetchIncorrectB() {
+    //port number hard coded - could pass in the PORT env variable?
+    const response = await fetch(`http://localhost:3005/api/quizquestions`);
+    const data = await response.json();
+    setIncorrectB(data.payload[0].incorrectanswerb);
+    // console.log(data.payload[0].correctanswer);
+  }
+
+  fetchIncorrectB();
+  // const rightAnswer = () => {
+  //   if (input === "1") {
+  //     alert("Correct");
+  //   } else {
+  //     alert("Wrong");
+  //   }
+  // };
+  // const handleChange = (e) => {
+  //   setInput(e.target.value);
+  //   console.log(input);
+  // };
+
+  // let answers = [1, 2, 3, 4];
+
   return (
-    <div>
-      <Dropdown
-        trigger={<button>Select Week</button>}
-        menu={[
-          <button onClick={handleMenuOne}>Week 1</button>,
-          <button onClick={handleMenuTwo}>Week 2</button>,
-          <button onClick={handleMenuThree}>Week 3</button>,
-          <button onClick={handleMenuFour}>Week 4</button>,
-        ]}
-      />
+    <div className="quiz-div">
       <div>
         <img
           src="https://media.giphy.com/media/3o7TKSjRrfIPjeiVyE/giphy.gif"
           alt="gif"
         />
         <p>{question}</p>
-      </div>
-      <div>
-        {answers.map((answer, key) => (
-          <div key={key}>
-            <input
-              type="radio"
-              id={answer}
-              name="wrong answer"
-              value={answer}
-              onChange={handleChange}
-            />
-            <label htmlFor={answer}>
-              ' Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-            </label>
-          </div>
-        ))}
-        <button onClick={rightAnswer}>Next</button>
+
+        <label>
+          <input type="radio" name="quiz" />
+          {correct}
+        </label>
+        <label>
+          <input type="radio" name="quiz" />
+          {incorrectA}
+        </label>
+        <label>
+          <input type="radio" name="quiz" />
+          {incorrectB}
+        </label>
+        <button>Submit Answer</button>
       </div>
     </div>
   );
 }
 
 export default QuizzMe;
+
+//Future idea 💡
+
+// const handleMenuOne = () => {
+//   console.log("clicked 1");
+// };
+
+// const handleMenuTwo = () => {
+//   console.log("clicked 2");
+// };
+// const handleMenuThree = () => {
+//   console.log("clicked 3");
+// };
+// const handleMenuFour = () => {
+//   console.log("clicked 4");
+// };
+// <Dropdown
+//         trigger={<button>Select Week</button>}
+//         menu={[
+//           <button onClick={handleMenuOne}>Week 1</button>,
+//           <button onClick={handleMenuTwo}>Week 2</button>,
+//           <button onClick={handleMenuThree}>Week 3</button>,
+//           <button onClick={handleMenuFour}>Week 4</button>,
+//         ]}
+//       />
