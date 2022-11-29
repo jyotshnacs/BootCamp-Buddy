0d781b970d33faad49182ca0aa62b5d2d455526c
lani-irving <laniirving@googlemail.com>
2022-11-29 12:24:38 +0000

    tidied up quizzme

--- src/components/Main/QuizzMe/QuizzMe.js
+++ src/components/Main/QuizzMe/QuizzMe.js
@@ -1,95 +1,48 @@
 import React from "react";
-// import Dropdown from "./Dropdown/Dropdown";
 import { useState } from "react";
 import "./QuizzMe.css";
 import questionMark from "../../../Images/question.png";
 import glasses from "../../../Images/glasses.png";
 import snippet from "../../../Images/snippet.png";
 
+// Creates a QuizzMe div.
 function QuizzMe() {
-  // const [input, setInput] = useState("");
   const [question, setQuestion] = useState("");
   const [correct, setCorrect] = useState("");
   const [incorrectA, setIncorrectA] = useState("");
   const [incorrectB, setIncorrectB] = useState("");
-  const [right, setRight] = useState(null);
-  const [wrong, setWrong] = useState(null);
 
-  /* 1) Send a fetch request to the Api
-   to get the quizQuestion back from the Database.*/
-
+  // Fetches and sets the question.
   async function fetchQuestion() {
-    //port number hard coded - could pass in the PORT env variable?
     const response = await fetch(`http://localhost:3005/api/quizquestions`);
     const data = await response.json();
     setQuestion(data.payload[0].question);
-    // console.log(data.payload[0].question);
   }
   fetchQuestion();
 
   async function fetchCorrect() {
-    //port number hard coded - could pass in the PORT env variable?
     const response = await fetch(`http://localhost:3005/api/quizquestions`);
     const data = await response.json();
     setCorrect(data.payload[0].correctanswer);
-    // console.log(data.payload[0].correctanswer);
   }
 
   fetchCorrect();
 
   async function fetchIncorrectA() {
-    //port number hard coded - could pass in the PORT env variable?
     const response = await fetch(`http://localhost:3005/api/quizquestions`);
     const data = await response.json();
     setIncorrectA(data.payload[0].incorrectanswera);
-    // console.log(data.payload[0].correctanswer);
   }
 
   fetchIncorrectA();
 
   async function fetchIncorrectB() {
-    //port number hard coded - could pass in the PORT env variable?
     const response = await fetch(`http://localhost:3005/api/quizquestions`);
     const data = await response.json();
     setIncorrectB(data.payload[0].incorrectanswerb);
-    // console.log(data.payload[0].correctanswer);
   }
 
   fetchIncorrectB();
-  // const rightAnswer = () => {
-  //   if (input === "1") {
-  //     alert("Correct");
-  //   } else {
-  //     alert("Wrong");
-  //   }
-  // };
-  // const handleChange = (e) => {
-  //   setInput(e.target.value);
-  //   console.log(input);
-  // };
-
-  // let answers = [1, 2, 3, 4];
-
-  // if the correct answer  is clicked right should be set to true
-  // if the wrong answer is clicked, right should be set to false
-  // HOW?!
-  // changing state onClick doesn't work because the state doesn't change UNTIL click,
-  // which means the next answer AFTER the correct one returns "correct!"
-  // we also need to eventually make it so that the answers are displayed in a different order each time!!!
-
-  // function checkAnswer() {
-  //   if (right === true) {
-  //       alert("correct!");
-  //         setRight(false);
-  //   } else if (right === false){
-  //     alert("incorrect!");
-  //   }
-  // }
-
-  // function correctAnswer(){
-  //   setRight(true);
-  //   console.log("correctAnswer");
-  // }
 
   return (
     <div className="quiz-div">
@@ -107,7 +60,7 @@ function QuizzMe() {
         alt="This content is not available"
         src={snippet}
       />
-
+      {/* Quiz alerts are hard coded as a placeholder until component is working */}
       <div className="question-container">
         <h2 className="question-here">{question}</h2>
         <div className="answer-container">
@@ -142,57 +95,3 @@ function QuizzMe() {
 }
 
 export default QuizzMe;
-
-//Future idea 💡
-
-// const handleMenuOne = () => {
-//   console.log("clicked 1");
-// };
-
-// const handleMenuTwo = () => {
-//   console.log("clicked 2");
-// };
-// const handleMenuThree = () => {
-//   console.log("clicked 3");
-// };
-// const handleMenuFour = () => {
-//   console.log("clicked 4");
-// };
-// <Dropdown
-//         trigger={<button>Select Week</button>}
-//         menu={[
-//           <button onClick={handleMenuOne}>Week 1</button>,
-//           <button onClick={handleMenuTwo}>Week 2</button>,
-//           <button onClick={handleMenuThree}>Week 3</button>,
-//           <button onClick={handleMenuFour}>Week 4</button>,
-//         ]}
-//       />
-
-/* {answers.map((answer, key) => (
-          <div key={key}>
-            <input
-              type="radio"
-              id={answer}
-              name="wrong answer"
-              value={answer}
-              onChange={handleChange}
-            />
-            <label htmlFor={answer}>
-              ' Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-            </label>
-          </div>
-        ))}
-        <button onClick={rightAnswer}>Next</button> */
-
-/* hard coded alerts because we ran out of time to get this working properly! */
-
-/* <p className="question-p">{question}</p>
-        <div className="answers" onClick={checkAnswer}>
-          {correct}
-        </div>
-        <div className="answers" onClick={checkAnswer}>
-          {incorrectA}
-        </div>
-        <div className="answers" onClick={checkAnswer}>
-
-          {incorrectB} */
