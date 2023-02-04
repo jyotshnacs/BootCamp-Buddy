ebc5bc1d5bae869af3281ef73bc84cfe7f8ea383
jo <jyotshnacs@gmail.com>
2023-02-04 11:27:23 +0000

    sorted

---
+++ Backend/models/quizQuestions.js
@@ -0,0 +1,16 @@
+const { query } = require("../db/index.js");
+
+async function getQuizQuestions() {
+  // Query the database and return all questions
+  // Use the query function from db/index.js
+  // Call the query function and pass in the SQL query
+  const result = await query("SELECT * FROM quizquestions");
+  // Return the result of the query
+  // Need to use await to wait for the query to finish
+  // await query("SQL query here");
+  return result.rows;
+}
+
+module.exports = {
+  getQuizQuestions,
+};
